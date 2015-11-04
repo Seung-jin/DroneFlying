ad06c9197ba4ce62653558e09bf1dae045e2fe4f
Seung-jin <fewfree12@naver.com>
2015-11-04 15:50:29 +0900

    졸업프로젝트 드론 조종기 어플 제작 시작

---
+++ app/src/androidTest/java/resolution/ex6/vr/droneflying/ApplicationTest.java
@@ -0,0 +1,13 @@
+package resolution.ex6.vr.droneflying;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
